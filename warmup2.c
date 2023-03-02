988120237ebd05ced9996aa2f146ce2ade8c13e4
APM1015 <122992723+APM1015@users.noreply.github.com>
2023-03-02 03:45:13 -0500

    Create warmup2.c

---
+++ warmup2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    // Open proc/meminfo file
+    FILE *filePath = fopen("/proc/meminfo", "r");
+
+    // checking if /proc/meminfo is NULL or contains an eror
+    if (filePath == NULL) {
+        printf("Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the contents of the file
+    char buf[1024];
+    while (fgets(buf, 1024, filePath) != NULL) {
+         printf("%s", buf);
+    }
+    // Close file
+    fclose(filePath);
+}
